456b4f0fccbfc49cc77a9fb85d5baae813ec2e00
fanxuekang <1175856464@qq.com>
2019-05-21 08:39:38 +0800

    1 Signed-off-by: fanxuekang <1175856464@qq.com>

---
+++ EWS/src/main/java/com.isoft.action/UserAction.java
@@ -0,0 +1,34 @@
+package com.isoft.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserAction {
+//    @RequestMapping("/login.do")
+    @RequestMapping("/login.do")
+    @ResponseBody
+    public String login(String uname, String upwd) {
+
+        /*硬编码*/
+
+        if(uname.equals("admin")&&upwd.equals("123456")) {
+            return "success";
+        }else{
+            return "fault";
+        }
+    }
+    @RequestMapping("/register.do")
+    @ResponseBody
+    public String register(String r_uname, String r_upwd,String email) {
+
+        /*硬编码*/
+        boolean temp=true;
+        if (temp)
+            return "success";
+        else
+            return "fault";
+    }
+}
