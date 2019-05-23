a2dca3d85a6e675f260c8ecb35b6d9c9921bf7a2
fanxuekang <1175856464@qq.com>
2019-05-23 10:29:58 +0800

    周三

--- EWS/src/main/java/com.isoft.action/UserAction.java
+++ EWS/src/main/java/com.isoft.action/UserAction.java
@@ -1,34 +1,53 @@
 package com.isoft.action;
 
+import com.isoft.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 @Controller
 @RequestMapping("/user")
 public class UserAction {
-//    @RequestMapping("/login.do")
+    @Autowired
+    IUserService userService;
     @RequestMapping("/login.do")
     @ResponseBody
     public String login(String uname, String upwd) {
-
-        /*硬编码*/
-
-        if(uname.equals("admin")&&upwd.equals("123456")) {
-            return "success";
-        }else{
-            return "fault";
-        }
+//        硬编码
+        String str = userService.login(uname,upwd);
+        return str;
     }
+
     @RequestMapping("/register.do")
     @ResponseBody
-    public String register(String r_uname, String r_upwd,String email) {
-
-        /*硬编码*/
-        boolean temp=true;
+    public String register(String uname, String upwd,String email) {
+//        硬编码
+        boolean temp = userService.register(uname,upwd,email);
         if (temp)
             return "success";
         else
             return "fault";
     }
+
+    @RequestMapping("/findAllUser.do")
+    @ResponseBody
+    public Map<String,Object> findAllUser(int page, int limit){
+//        硬编码
+        List<Map<String,Object>> list=userService.findAllUser(page,limit);
+//构造能被layui认识的的返回数据格式
+        Map<String, Object> userCount = userService.findUserCount();
+        Map map=new HashMap();
+        map.put("code",0);
+        map.put("msg","用户信息数据");
+        map.put("count",userCount.get("count"));  //用户表的总记录数
+        map.put("data",list);
+        return map;                       /*返回json格式数据，但是不能转换，因为找不到json消息转化器*/
+    }
 }
+
